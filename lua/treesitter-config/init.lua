4f0c464ff1879d99de75456757ff8704ed6d5286
sansui <872115355@qq.com>
2022-01-29 11:06:46 +0800

    config sansui

---
+++ lua/treesitter-config/init.lua
@@ -0,0 +1,13 @@
+Vapour.utils.plugins.packadd('nvim-treesitter')
+
+Vapour.utils.plugins.require'nvim-treesitter.install'.compilers = {"gcc"}
+
+Vapour.utils.plugins.require'nvim-treesitter.configs'.setup {
+  --ensure_installed = Vapour.plugins.treesitter.ensure_installed, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = {"html", "css", "vim", "lua", "javascript", "typescript", "tsx"},
+  ignore_install = Vapour.plugins.treesitter.ignore_install,
+  indent = Vapour.plugins.treesitter.indent,
+  highlight = Vapour.plugins.treesitter.highlight,
+  autotag = Vapour.plugins.treesitter.autotag,
+  rainbow = Vapour.plugins.treesitter.rainbow
+}
